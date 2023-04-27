4b46e2635258a2a9796ebac0ff6f5f42c312d1bb
fedmarti <fedmarti@student.42firenze.it>
2023-04-28 01:28:50 +0200

    header

---
+++ minitalk.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: federico <federico@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 21:36:11 by federico          #+#    #+#             */
+/*   Updated: 2023/04/28 01:05:41 by federico         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# ifndef _XOPEN_SOURCE
+#  define _XOPEN_SOURCE 700
+# endif
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include "ft_printf/ft_printf.h"
+
+struct s_c_list
+{
+	struct s_c_list	*next;
+	char			c;
+};
+
+struct s_c_list	*node_init(void);
+struct s_c_list	*add_node(struct s_c_list *head, struct s_c_list *node);
+void			print_list(struct s_c_list *node);
+void			free_list(struct s_c_list *head);
+
+#endif
