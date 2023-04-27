42c67ae1cfe9e9670b3251a37cdbec61fdaa21b3
fedmarti <fedmarti@student.42firenze.it>
2023-04-28 01:31:10 +0200

    project nearing its state of completion

--- server.c
+++ server.c
@@ -6,58 +6,67 @@
 /*   By: federico <federico@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 23:50:25 by federico          #+#    #+#             */
-/*   Updated: 2023/04/26 00:08:34 by federico         ###   ########.fr       */
+/*   Updated: 2023/04/28 01:15:33 by federico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <signal.h>
+#include "minitalk.h"
 
-char	c;
-
-void	check_char(void)
+struct s_c_list	*add_bit(int bit)
 {
-	if (!c)
-		exit(0);
-	else
-		write(1, &c, 1);
-}
-
-void	add_bit(int	bit)
-{
-	static int	bit_pos;
+	static int					bit_pos;
+	static struct s_c_list		*c;
 
 	if (!bit_pos || bit_pos == 1)
 	{
-		c = 0;
+		c = node_init();
+		if (!c)
+			return (NULL);
 		bit_pos = 1;
 	}
-	c = c | (bit_pos * bit);
-	if (bit_pos == 10000000)
+	c->c = c->c | (bit_pos * bit);
+	if (bit_pos == 128)
 	{
 		bit_pos = 1;
-		check_char();
+		return (c);
 	}
-	else
-		bit_pos = bit_pos << 1;	
+	bit_pos = bit_pos << 1;
+	return (NULL);
 }
 
-void	handle_sigusr1(void)
+void	ft_sig_handler(int sig, siginfo_t *info, void *ucontext)
 {
-	add_bit(0);
+	static struct s_c_list	*list;
+	struct s_c_list			*temp_node;
+
+	(void)sig;
+	(void)ucontext;
+	temp_node = NULL;
+	if (info->si_signo == SIGUSR1)
+		temp_node = add_bit(0);
+	else if (info->si_signo == SIGUSR2)
+		temp_node = add_bit(1);
+	if (temp_node)
+		list = add_node(list, temp_node);
+	if (temp_node && temp_node->c == 0)
+	{
+		print_list(list);
+		kill(info->si_pid, SIGUSR1);
+		free_list(list);
+		list = NULL;
+	}
+	kill(info->si_pid, SIGUSR2);
 }
 
-void	handle_sigusr2(void)
+int	main(void)
 {
-	add_bit(1);
-}
+	struct sigaction	act;
 
-int	main()
-{
-	sigaction(SIGUSR1, &handle_sigusr1);
-	sigaction(SIGUSR2, &handle_sigusr2);
-
-	ft_printf("pid = %u", getgid());
-	pause()
+	act.sa_sigaction = &ft_sig_handler;
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+	ft_printf("Server pid = %u\n", getpid());
+	while (1)
+		pause();
 }
